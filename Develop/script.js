2a240f2fda2d816aaccba8ab8e8e889983f01af0
unknown <aszonyi49@gmail.com>
2021-09-23 13:53:56 +1000

    Created two arrays for buttons and inputs, added for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,97 @@
 // handle displaying the time
-var rootEl = $('#root');
-var Container = $('.container');
-var Group1 = $('#input-group1');
-var Group2 = $('#input-group2');
-var Group3 = $('#input-group3');
-var Group4 = $('#input-group4');
-var Group5 = $('#input-group5');
-var Group6 = $('#input-group6');
-var Group7 = $('#input-group7');
-var Group8 = $('#input-group8');
-var Group9 = $('#input-group9');
-var grouppp = [Group1, Group2, Group3, Group4, Group5, Group6, Group7, Group8, Group9]
-var timeDisplayEl = $('#time-display');
-var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-var DivTime = $('.form-control');
-var input11 = $('#11');
-var Button11 = $('Button11')
+var rootEl = $("#root");
+var Container = $(".container");
+var Group1 = $("#input-group1");
+var Group2 = $("#input-group2");
+var Group3 = $("#input-group3");
+var Group4 = $("#input-group4");
+var Group5 = $("#input-group5");
+var Group6 = $("#input-group6");
+var Group7 = $("#input-group7");
+var Group8 = $("#input-group8");
+var Group9 = $("#input-group9");
+var grouplist = [
+  Group1,
+  Group2,
+  Group3,
+  Group4,
+  Group5,
+  Group6,
+  Group7,
+  Group8,
+  Group9,
+];
 
-var hour = moment().format('HH')
-var hour1 = $('#hour1');
-var hour2 = $('#hour2');
-var hour3 = $('#hour3');
-var hour4 = $('#hour4');
-var hour5 = $('#hour5');
-var hour6 = $('#hour6');
-var hour7 = $('#hour7');
-var hour8 = $('#hour8');
-var hour9 = $('#hour9');
-var hoursss = [hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9]
+var timeblock = $(".timeblock")
+var timeDisplayEl = $("#time-display");
+var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+var DivTime = $(".form-control");
+var input9 = $("#input-group1")[0];
+var input10 = $("#input-group2")[0];
+var input11 = $("#input-group3")[0];
+var input12 = $("#input-group4")[0];
+var input13 = $("#input-group5")[0];
+var input14 = $("#input-group6")[0];
+var input15 = $("#input-group7")[0];
+var input16 = $("#input-group8")[0];
+var input17 = $("#input-group9")[0];
+var inputlist = [input9, input10, input11, input12, input13, input14, input15, input16, input17]
+var Button9 = $("#Button09");
+var Button10 = $("#Button10");
+var Button11 = $("#Button11");
+var Button12 = $("#Button12");
+var Button13 = $("#Button13");
+var Button14 = $("#Button14");
+var Button15 = $("#Button15");
+var Button16 = $("#Button16");
+var Button17 = $("#Button17");
+var Buttonlist = [Button9, Button10, Button11, Button12, Button13, Button14, Button15, Button16, Button17]
+
+var hourtime = moment().format("HH");
+var hour1 = $("#hour1");
+var hour2 = $("#hour2");
+var hour3 = $("#hour3");
+var hour4 = $("#hour4");
+var hour5 = $("#hour5");
+var hour6 = $("#hour6");
+var hour7 = $("#hour7");
+var hour8 = $("#hour8");
+var hour9 = $("#hour9");
+var hourlist = [hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8, hour9];
 var todoList = $("#todo-list");
-var storedTodos = JSON.parse(localStorage.getItem("todos"));
+// var storedTodos = JSON.parse(localStorage.getItem("todos"));
 
-console.log(hoursss);
+
+
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
-  displayTime();
-  console.log(rightNow);
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+console.log(rightNow);
 
-var todos = [];
+var todos = {
+  0: '',
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+  5: '',
+  6: '',
+  7: '',
+  8: '',
+};
 
 // The following function renders items in a todo list as <li> elements
 function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  input11.innerHTML = "";
-  input11.textContent = storedTodos;
+  // Clear todoList element
+  // // input11.innerHTML = "";
+  // input11.textContent = todos;
+  for (var i = 0; i < Buttonlist.length; i++) {
+    inputlist[i].value = todos[i]
+
+}
 }
 // This function is being called below and will run when the page loads.
 function init() {
@@ -62,62 +107,40 @@ function init() {
   renderTodos();
 }
 
-function storeTodos() {
+function storeTodos(hour, hourtext) {
   // Stringify and set key in localStorage to todos array
-  input11.value = localStorage.setItem("todos", JSON.stringify(todos));
+  // input11.value = localStorage.setItem("todos", JSON.stringify(todos));
+  todos[hour] = hourtext;
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
-
-// Add submit event to form
-Button11.on("click", function(event) {
+for (var i = 0; i < Buttonlist.length; i++) {
+Buttonlist[i].on("click", function (event) {
   event.preventDefault();
 
-  var todoText = input11.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
-  }
+  var todoText = inputlist[i].value.trim();
+ 
 
   // Add new todoText to todos array, clear the input
-  todos.push(input11.value);
-  todoInput.value = "";
+  // input11.value = "";
 
   // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
+  storeTodos(i, todoText);
+  // renderTodos();
+
 });
+}
 
 // Add click event to todoList element
 
 // Calls init to retrieve data and render it to the page on load
-init()
+init();
 
-
-
-// group should be an array
-// time variable should be an array
-// loop through one and check to compare the hour time to the moment time and then apply
-
-for (var i = 0; i < hoursss.length; i++) {
-
-  if (hoursss[i].attr('data-hour') < hour) {
-    grouppp[i].addClass('past');
+for (var i = 0; i < hourlist.length; i++) {
+  if (hourlist[i].attr("data-hour") < hourtime) {
+    grouplist[i].addClass("past");
+  } else if (hourtime === hourlist[i].attr("data-hour")) {
+    grouplist[i].addClass("present");
+  } else {
+    grouplist[i].addClass("future");
   }
-  else if (hour === hoursss[i].attr('data-hour')) {
-    grouppp[i].addClass('present');
-  }
-else {
-  grouppp[i].addClass('future');
 }
-}
-console.log(hour);
-
-
-
-//add a timer at the top that shows current time
-//display containers for all work hours 8am to 8pm
-//allow users to click on the container and present a form
-//form will have input field for todo tasks
-//append form input on to container
-//change colours of previous containers based on what time it is now 
-//button to clear todo from container
